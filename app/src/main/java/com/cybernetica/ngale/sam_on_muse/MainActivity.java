70b6f7aa63cbcf06b28e47409af0124a9bb1e9e6
Valkaryne <anix.mig29@gmail.com>
2018-12-31 19:29:52 +0300

    Installed Muse-library to the project

--- app/src/main/java/com/cybernetica/ngale/sam_on_muse/MainActivity.java
+++ app/src/main/java/com/cybernetica/ngale/sam_on_muse/MainActivity.java
@@ -2,12 +2,18 @@ package com.cybernetica.ngale.sam_on_muse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.choosemuse.libmuse.MuseManagerAndroid;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MuseManagerAndroid manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = MuseManagerAndroid.getInstance();
+        manager.setContext(this);
     }
 }
